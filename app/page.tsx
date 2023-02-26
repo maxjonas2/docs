ff1eaa719e31c133224c7937d191a93102f4852e
maxjonas2 <maxjonas2@hotmail.com>
2023-02-25 23:43:58 -0300

    Added search

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import {
+  HTMLAttributes,
+  HtmlHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 const sectionTitles = [
   "About",
@@ -9,13 +15,15 @@ const sectionTitles = [
   "Contact",
   "Footer",
   "Header",
+  "Contract One",
+  "Contract Two",
 ];
 
 export default function Home() {
   const lorem =
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est accusantium eius ipsum vel laudantium dolorum aspernatur quasi odit. Libero fugit ipsa quod totam quis, dicta eaque neque deleniti eligendi deserunt recusandae incidunt hic tempora, modi, natus adipisci atque repudiandae quam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Est accusantium eius ipsum vel laudantium dolorum aspernatur quasi odit. Libero fugit ipsa quod totam quis, dicta eaque neque deleniti eligendi deserunt recusandae incidunt hic tempora, modi, natus adipisci atque repudiandae quam.";
 
-  const [currentSection, setCurrentSection] = useState<String | null>(
+  const [currentSection, setCurrentSection] = useState<string>(
     sectionTitles[0]
   );
   const sectionContainer = useRef<HTMLDivElement>(null);
@@ -46,22 +54,7 @@ export default function Home() {
 
   return (
     <>
-      <nav className="sub-nav">
-        <h2 className="font-bold">Sub-Content</h2>
-        {sectionTitles.map((title) => {
-          return (
-            <div
-              className={
-                title === currentSection
-                  ? "mt-2 text-md font-bold"
-                  : "mt-2 text-md font-normal"
-              }
-            >
-              <a href={`#${title}`}>{title}</a>
-            </div>
-          );
-        })}
-      </nav>
+      <SubNav sectionTitles={sectionTitles} currentSection={currentSection} />
       <div
         ref={sectionContainer}
         className="main-content flow h-screen scroll-smooth overflow-y-scroll"
@@ -72,10 +65,9 @@ export default function Home() {
               <h1 id={title} className="text-lg font-bold">
                 {title}
               </h1>
-              <p>{lorem}</p>
-              <p>{lorem}</p>
-              <p>{lorem}</p>
-              <p>{lorem}</p>
+              {[1, 2, 3, 4, 5].map((item) => (
+                <p>{lorem}</p>
+              ))}
             </section>
           );
         })}
@@ -83,3 +75,52 @@ export default function Home() {
     </>
   );
 }
+
+const SubNav: React.FC<{ sectionTitles: string[]; currentSection: string }> = ({
+  sectionTitles,
+  currentSection,
+}) => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const filteredSections = sectionTitles.filter((title) =>
+    title.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
+  return (
+    <nav className="sub-nav">
+      <h2 className="font-bold">Sub-Content</h2>
+      <TextInput
+        value={searchValue}
+        onChange={(e) => setSearchValue((e.target as HTMLInputElement).value)}
+      />
+      {filteredSections.map((title) => {
+        return (
+          <div
+            className={
+              title === currentSection
+                ? "mt-2 text-md font-bold"
+                : "mt-2 text-md font-normal"
+            }
+          >
+            <a href={`#${title}`}>{title}</a>
+          </div>
+        );
+      })}
+    </nav>
+  );
+};
+
+const TextInput: React.FC<React.HTMLProps<HTMLInputElement>> = ({
+  ...props
+}) => {
+  return (
+    <div className="w-[90%] rounded-sm border border-slate-400/60 h-[2rem]">
+      <input
+        {...props}
+        type="text"
+        className=" indent-1 text-sm h-full w-full border-none text-black/80"
+        placeholder="Pesquisar"
+      />
+    </div>
+  );
+};
